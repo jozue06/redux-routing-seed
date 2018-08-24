7ee43ca7cd4b5abcbc55e6feb97d32f7037565a8
JB Tellez <jb.tellez@gmail.com>
2018-08-23 18:07:51 -0700

    fails attempt at REDUX dev tools integration

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import thingReducer from './thing';
 
-export default createStore(combineReducers({thingState: thingReducer}), applyMiddleware(thunk));
+const appReducer = combineReducers({thingState: thingReducer});
+
+export default createStore(appReducer, applyMiddleware(thunk));
+
+// Can't get to work with REDUX Dev tools :(
+  
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// export default () => createStore(appReducer, composeEnhancers(applyMiddleware(thunk)));
